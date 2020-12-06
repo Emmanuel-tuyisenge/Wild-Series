e2ca6c3dbacfd68320c5dd580f948dfeb3fb1f76
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2020-12-06 16:26:49 +0100

    Retrieve data stored with Doctrine and create two display pages

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,29 +4,47 @@
 namespace App\Controller;
 
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 
     /**
-     * @Route("/programs/{id<\d+>}", methods={"GET"}, name="program_show")
+     * @Route("/show/{id<\d+>}", methods={"GET"}, name="show")
      */
     public function show(int $id): Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' .$id. ' found in program\'s table.'
+            );
+        }
+
         return $this->render('program/show.html.twig', [
-            'id' => $id
+            'program' => $program
         ]);
     }
 }
