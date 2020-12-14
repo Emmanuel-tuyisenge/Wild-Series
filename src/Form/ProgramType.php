85c73700b59bc280decfc98d7cf0902877d2125d
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2020-12-14 18:13:19 +0100

    Added actor field in ProgramType and redrafted new.twig

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Category;
 use App\Entity\Program;
 
@@ -55,6 +56,14 @@ class ProgramType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'name'
             ])
+            ->add('actors', EntityType::class, [
+                'label' => 'Acteur',
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
         ;
     }
 
